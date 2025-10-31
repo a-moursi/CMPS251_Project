e9d961d688ee5192b905399706d9f077fbe5f89d
Khan70412 <anaskhan.aak92@gmail.com>
2025-10-31 13:55:41 +0300

    Test

--- WareHouse/src/App.java
+++ WareHouse/src/App.java
@@ -6,6 +6,7 @@ public class App {
 	public static final LocalDate TODAY = LocalDate.of(2025, 10, 24);
 	public static void main(String[] args) {
 		System.out.println("Works ?");
+		System.out.println("Works ?");
 	}
 	
 }
