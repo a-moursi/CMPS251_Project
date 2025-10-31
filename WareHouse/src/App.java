ab90bd4dd20162eccda066ca09e1c71f38801a94
Khan70412 <anaskhan.aak92@gmail.com>
2025-10-31 13:30:56 +0300

    Test

--- WareHouse/src/App.java
+++ WareHouse/src/App.java
@@ -1,9 +1,11 @@
 import java.time.LocalDate;
 
+
+
 public class App {
 	public static final LocalDate TODAY = LocalDate.of(2025, 10, 24);
 	public static void main(String[] args) {
-		
+		System.out.println("Works ?");
 	}
 	
 }
