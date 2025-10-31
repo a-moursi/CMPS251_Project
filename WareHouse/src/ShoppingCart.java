8aaa9d88624ce701f252dab33d5899fbe9fe22f2
Khan70412 <anaskhan.aak92@gmail.com>
2025-10-31 23:04:31 +0300

    Update ShoppingCart.java

--- WareHouse/src/ShoppingCart.java
+++ WareHouse/src/ShoppingCart.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
 
 public class ShoppingCart {
+	private ArrayList<CartItem> items = new ArrayList<>();
 
+	/**
+	 * @return the items
+	 */
+	public ArrayList<CartItem> getItems() {
+		return items;
+	}
+
+	public void addItem(Product product, int quantity) {
+		
+	}
+	
+	public void removeIndex(int index) {
+		
+	}
+	
+	public boolean isEmpty() {
+		return true;
+	}
+	
+	public void clear() {
+		
+	}
+	public double subtotal() {
+		return 0.0;
+	}
+	
+	public double totalWeight() {
+		return 0.0;
+	}
+	
+	public void print() {
+		
+	}
+	
+	
+	public ArrayList<OrderItem> toOrderItems(){
+		return null;
+	}
+	
+	
+	
 }
