663bc44327be98c6ab2cb250f305963af5e1f7ac
Rexyfr <iams11223344@gmail.com>
2025-11-01 13:31:20 +0300

    DON'T FORGET TO PULL FIRST

--- WareHouse/src/Discount.java
+++ WareHouse/src/Discount.java
@@ -1,104 +1,23 @@
 import java.time.LocalDate;
 
-<<<<<<< HEAD
-public class Discount {
-	//<<properties>>??
-=======
+
 public abstract class Discount {
 	
->>>>>>> 0b00db0b9b19727af4f3a2d504f80593a6595f45
+
 	private String code;
 	private LocalDate startDate;
 	private LocalDate endDate;
 	private boolean active;
 	
-<<<<<<< HEAD
-	//----------------------------------------------------
-	
-	public Discount(String code, LocalDate start, LocalDate end, boolean active) {
+
+	protected Discount(String code, LocalDate start, LocalDate end, boolean active) {
 		super();
-		this.setCode(code);
-		this.setStartDate(start);
-		this.setEndDate(end);
-		this.setActive(active);
-	}
-	
-	//----------------------------------------------
-
-	public String getCode() {
-		return code;
-	}
-
-
-	public void setCode(String code) {
 		this.code = code;
-	}
-
-
-	public LocalDate getStartDate() {
-		return startDate;
-	}
-
-
-	public void setStartDate(LocalDate startDate) {
-		this.startDate = startDate;
-	}
-
-
-	public LocalDate getEndDate() {
-		return endDate;
-	}
-
-
-	public void setEndDate(LocalDate endDate) {
-		this.endDate = endDate;
-	}
-
-
-	public boolean isActive() {
-		return active;
-	}
-
-
-	public void setActive(boolean active) {
+		startDate = start;
+		endDate = end;
 		this.active = active;
 	}
 	
-	//---------------------------------------
-	
-	public double calculateDiscount(double subtotal) {
-		double result = 0;
-		
-		return result;
-	}
-	
-	public String getDetailsShort() {
-		String result = null;
-		
-		return result;
-	}
-	
-	public String getDetails() {
-		String result = null;
-		
-		return result;
-	}
-	
-	protected String detailsTail() {
-		String tail = null;
-		
-		return tail;
-	}
-	
-	//----------------
-	
-	public static boolean overlaps(Discount a, Discount b) {
-		boolean overlap = false;
-		
-		return overlap;
-	}
-	
-=======
 	public String getCode() {
 		return code;
 	}
@@ -118,14 +37,6 @@ public abstract class Discount {
 		active = a;
 	}
 	
-	 
-	protected Discount(String code, LocalDate start, LocalDate end, boolean active) {
-		super();
-		this.code = code;
-		startDate = start;
-		endDate = end;
-		this.active = active;
-	}
 
 	 public double calculateDiscount(double subtotal) {
 		 
@@ -160,8 +71,5 @@ public abstract class Discount {
 		 
 	 
 	 
-	 
-	 
->>>>>>> 0b00db0b9b19727af4f3a2d504f80593a6595f45
-	
+	 	
 }
